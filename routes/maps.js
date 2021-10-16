3f51c0229d953a445b6a5ef2d6e7dbdf494716c8
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-10-16 17:25:22 -0400

    get map routes

---
+++ routes/maps.js
@@ -0,0 +1,39 @@
+/*
+ * All routes for Widgets are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+module.exports = function(router, database) {
+  router.get("/", (req, res) => {
+    database.getAllMaps()
+      .then(maps => {
+        res.send({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  //if authenticated user - user created maps
+  //can change route name
+  router.get("/:id", (req, res) => {
+    //dummy
+    userId = 1;
+    database.getAllUserMaps(userId)
+      .then(maps => {
+        res.send({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+
+  return router;
+};
