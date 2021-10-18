9c620d69cf761c6601dae1fe122413232223e57c
niubrandon <niubrandon@gmail.com>
2021-10-18 17:14:54 -0400

    add map feature

--- routes/maps.js
+++ routes/maps.js
@@ -6,6 +6,8 @@
  */
 
 
+
+
 module.exports = function(router, database) {
 
   router.get("/", (req, res) => {
@@ -70,6 +72,54 @@ module.exports = function(router, database) {
         .json({ error: err.message });
     });
   });
+
+
+
+/*
+find user information from username
+user is an object may contain username and password
+*/
+  const userInformation = function(user) {
+    return database.findUserFromUsername(user).then(data => {
+      console.log("finding user from finduser query", data.id);
+      if (data) {
+        return data.id;
+      } else {
+        return null;
+      }
+    });
+  };
+
+  exports.userInformation = userInformation;
+
+
+
+  /*
+  POST on create a map endpoint
+  :id is username from loggedin user
+  check cookie const userName = req.session.userName;
+  find userId from username nested promise
+  */
+
+  router.post("/:id/addmap", (req, res) => {
+    //check cookie first
+    const userName = req.session.userName;
+    const user = {
+      username: userName
+    };
+    console.log("logged in as username from cookie", userName);
+    userInformation(user).then(userId => {
+      database.addMapFromAuthUser(req.body, userId).then(data => {
+        res.status(202).send({data});
+      }).catch(err => {
+        res.status(500).json({error: "error"});
+      });
+
+    }).catch(err => {
+    });
+
+  });
+
 
   return router;
 };
--- routes/users.js
+++ routes/users.js
@@ -34,13 +34,10 @@ module.exports = function(router, database) {
   const login = function(user, password) {
     console.log("function called login");
     return database.findUserFromUsername(user).then(data => {
-      console.log("finding user from finduser query", data);
       if (data) {
         if (bcrypt.compareSync(password, data.password)) {
-          console.log("user is in database", data);
           return data;
         } else {
-          console.log("user is not in the database");
           return null;
         }
       }
@@ -55,10 +52,8 @@ module.exports = function(router, database) {
   user registration route
   --fix edge case later if username and email is already in the database
   */
-  //brandon addUser feature
   router.post('/register', (req, res) => {
     const user = req.body;
-    console.log("recevied post ajax request from registration",user);
     user.password = bcrypt.hashSync(req.body.password, salt);
     database.addUser(user)
       .then(data => {
@@ -66,7 +61,6 @@ module.exports = function(router, database) {
           res.send({error: "error"});
           return;
         }
-        console.log("register user and added into database", data);
         req.session.userName = data.username;
         res.send({user:{name: data.name, email: data.email, password: data.password}});
 
@@ -81,10 +75,7 @@ module.exports = function(router, database) {
   */
   router.post('/login', (req, res) => {
     const {username, email, password} = req.body;
-
-    console.log("received post ajax request from user login", req.body);
     login(req.body, req.body.password).then(data => {
-      console.log("return after the password check", data);
       if (!data) {
         res.send({error: "error"});
         return;
@@ -95,7 +86,6 @@ module.exports = function(router, database) {
     })
       .catch(e => res.send(e));
   });
-
 
   router.post('/logout', (req, res) => {
     req.session = null;
@@ -118,35 +108,6 @@ module.exports = function(router, database) {
           .json({ error: err.message });
       });
   });
-
-
-  //obselete code. doesn't return the json object to client
-  //check dababase if user exist and check password
-  /*  database.findUserFromUsername(user).then(data => {
-      //check if user if it exits verify the return from db if it is undefine
-      if (!data) {
-        console.log("user is not existed in database");
-        res.send({error: "error"});
-        return;
-      } else {
-      //check userpassword
-        console.log("result from database when user login", userPassword, data.password,data);
-        if (bcrypt.compareSync(userPassword,data.password)) {
-
-          //create session cookie
-          req.session.userName = data.name;
-          res.status(201).send({data: {name: data.name, email: data.email, password: data.password}});
-          console.log("correct password");
-        } else {
-          console.log("wrong password");
-          res.status(401).send("password is wrong");
-        }
-      }
-
-    }).catch(e => res.send(e));
-  //create a session cookie
-  });
-*/
 
   return router;
 };
