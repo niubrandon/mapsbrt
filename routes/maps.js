f6e0c36e55e2b121ff1e5c253ff0b06e76859115
Tom Zhang <tom.zhang989@gmail.com>
2021-10-19 20:31:32 -0400

    adds usermaps

--- routes/maps.js
+++ routes/maps.js
@@ -74,16 +74,15 @@ module.exports = function(router, database) {
 
   // Post a new point of single map
   router.post("/:id/points", (req, res) => {
+    console.log('posting point');
     database.postPointsbyMapID(req.params.id)
       .then(
         points => {
-          const pointData = req.body;
-          console.log('posting point', req.body.point_title);
-          // console.log('posting points param',req.params.id);
+          // console.log('points param',req.params.id);
           res.send({ points });
         })
       .catch(err => {
-        console.log('single map err?');
+        // console.log('single map err?');
         res
           .status(500)
           .json({ error: err.message });
@@ -91,23 +90,85 @@ module.exports = function(router, database) {
   });
 
 
-  //if authenticated user - user created maps
-  router.get("/usermaps", (req, res) => {
-    userId = req.session.userId;
-    if (!userId) {
-      res.error("Not found");
-      return;
-    }
-    database.getAllUserMaps(userId)
-      .then(maps => {
-        res.send({ maps });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+
+
+  /*
+find user information from username
+user is an object may contain username and password
+*/
+  const userInformation = function(user) {
+    return database.findUserFromUsername(user).then(data => {
+      console.log("finding user from finduser query", data.id);
+      if (data) {
+        return data.id;
+      } else {
+        return null;
+      }
+    });
+  };
+
+  exports.userInformation = userInformation;
+
+
+
+  /*
+  POST on create a map endpoint
+  :id is username from loggedin user
+  check cookie const userName = req.session.userName;
+  find userId from username nested promise
+  */
+
+  router.post("/:id/addmap", (req, res) => {
+    //check cookie first
+    const userName = req.session.userName;
+    const user = {
+      username: userName
+    };
+    console.log("logged in as username from cookie", userName);
+    userInformation(user).then(userId => {
+      database.addMapFromAuthUser(req.body, userId).then(data => {
+        res.status(202).send({data});
+      }).catch(err => {
+        res.status(500).json({error: "error"});
       });
+
+    }).catch(err => {
+    });
+
   });
+
+
+  /*
+POST on delete a map endpoint
+:id is username from auth user
+:mapId is the map id belong to the auth user
+need map id from ajax post call
+*/
+
+  router.post("/:id/deletemap/:mapid", (req, res) => {
+    //check cookie first
+    const mapId = req.params.mapId;
+    const userName = req.session.userName;
+    console.log("ajax post data is deleting a map is", req.body);
+    database.deleteMapFromAuthUser(mapId).then(data => {
+      if (data) {
+        res.status(201).send({data});
+      }
+    }).catch(err => {
+      res.send({error: "error on delete map post request"});
+    });
+
+  });
+
+  /*
+POST on modify a map endpoint
+:id is username from auth user
+:mapId is the map id belong to the auth user
+need map id from ajax post call
+need to submit all the changes
+*/
+
+
 
 
 
